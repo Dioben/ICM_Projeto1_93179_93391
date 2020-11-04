8c016e9ddb29c160a03a332a7ed0e77a3d52307f
buckaroo69 <diogoobento2000@gmail.com>
2020-11-04 13:44:13 +0000

    initial commit

---
+++ app/src/main/java/com/example/icm_projeto1_93179_93391/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.icm_projeto1_93179_93391;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
