b706209e707afc95a1a8c88f2e10fc9c2b63c9b2
buckaroo69 <diogoobento2000@gmail.com>
2020-11-17 17:11:48 +0000

    google login

--- app/src/main/java/com/example/icm_projeto1_93179_93391/ui/MainActivity.java
+++ app/src/main/java/com/example/icm_projeto1_93179_93391/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.icm_projeto1_93179_93391.ui;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,14 +16,24 @@ import android.widget.Toast;
 
 import com.example.icm_projeto1_93179_93391.R;
 import com.example.icm_projeto1_93179_93391.network.FirebaseQueryClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN =1 ;
     private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         auth.signInWithEmailAndPassword(mail,pw).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
+                Toast.makeText(getApplication(),"Logged in",Toast.LENGTH_SHORT).show();
                 FirebaseQueryClient.getInstance().setUser(authResult.getUser());
                 Intent start = new Intent(getApplication(),main_menu.class);
                 startActivity(start);
@@ -70,6 +82,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void googlesign_onClick(View view) {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                auth.signInWithCredential(credential).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Google and our Servers failed to cooperate", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        if(!authResult.getAdditionalUserInfo().isNewUser()){
+                            Toast.makeText(getApplication(),"Signed in with Google",Toast.LENGTH_SHORT).show();
+                            FirebaseQueryClient.getInstance().setUser(authResult.getUser());
+                            Intent main = new Intent(getApplication(),main_menu.class);
+                            startActivity(main);
+                        }
+                        expandUI(authResult.getUser());
+                    }
+                });
+            } catch (ApiException e) {
+                Toast.makeText(this,"Google Sign in failed",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void expandUI(FirebaseUser usr){
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.activity_username_popup, null);
 
@@ -79,12 +136,27 @@ public class MainActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 true
         );
+        TextInputEditText namebox = findViewById(R.id.user_name_input);
 
         Button confirm_upload_button = (Button) popupView.findViewById(R.id.confirm_username_button);
         confirm_upload_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                logbutton_onClick(v);
+                String name = namebox.getText().toString();
+                UserProfileChangeRequest request = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+                usr.updateProfile(request).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplication(),"name setting failed, please try again",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        FirebaseQueryClient.getInstance().setUser(auth.getCurrentUser());
+                        Intent start = new Intent(getApplication(),main_menu.class);
+                        startActivity(start);
+                    }
+                });
             }
         });
 
