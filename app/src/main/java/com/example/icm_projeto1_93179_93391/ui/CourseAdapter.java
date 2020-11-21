64054f752a0376bc2b07ad610ea268eb7d5297f4
p-laranjinha <plcasimiro@ua.pt>
2020-11-21 17:33:15 +0000

    Work on course list

--- app/src/main/java/com/example/icm_projeto1_93179_93391/ui/CourseAdapter.java
+++ app/src/main/java/com/example/icm_projeto1_93179_93391/ui/CourseAdapter.java
@@ -3,9 +3,11 @@ package com.example.icm_projeto1_93179_93391.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.icm_projeto1_93179_93391.R;
 import com.example.icm_projeto1_93179_93391.datamodel.Course;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
@@ -35,8 +38,10 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
-    Course current = courses.get(position);
-        holder.dataholder.setText(current.name + " at "+current.getFormattedTimestamp());
+        Course current = courses.get(position);
+        holder.course_name.setText(current.name);
+        holder.owner_name.setText(current.getUser());
+        holder.date_uploaded.setText(DateUtils.getRelativeTimeSpanString(current.getTimestamp(), Calendar.getInstance().getTimeInMillis(), DateUtils.MINUTE_IN_MILLIS));
     }
 
     @Override
@@ -45,10 +50,11 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     }
 
     public class CourseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView dataholder;
-        //UI elements here
-        //
-        //
+        LinearLayout course_item;
+        TextView course_name;
+        TextView owner_name;
+        TextView date_uploaded;
+
         CourseAdapter mAdapter;
         public CourseViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -57,8 +63,11 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
         public CourseViewHolder(View mItemView, CourseAdapter courseAdapter) {
             super(mItemView);
             mAdapter = courseAdapter;
-            dataholder = mItemView.findViewById(R.id.course_item);
-            dataholder.setOnClickListener(this);
+            course_item = mItemView.findViewById(R.id.course_item);
+            course_name = mItemView.findViewById(R.id.course_name);
+            owner_name = mItemView.findViewById(R.id.owner_name);
+            date_uploaded = mItemView.findViewById(R.id.date_uploaded);
+            course_item.setOnClickListener(this);
         }
 
         @Override
